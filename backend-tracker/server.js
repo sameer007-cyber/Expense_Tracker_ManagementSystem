f78672ee50d6a3f401b89cd28fc800f10c980f44
Sameer  Dharmadhikari <sameerdharmadhikari21@gmail.com>
2026-04-14 19:29:41 +0530

    fix cors properly

--- backend-tracker/server.js
+++ backend-tracker/server.js
@@ -15,8 +15,11 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: "https://expense-tracker-managementsystem.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+app.options("*", cors());
 
 // connect database
 connectDB();
